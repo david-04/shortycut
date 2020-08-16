1543564fc8010e581f9229d01422b909acfd84e5
David Hofmann <65802756+david-04@users.noreply.github.com>
2020-08-16 10:32:35 +1000

    Added release 1.0

--- src/docs/release-notes.md
+++
@@ -1,5 +0,0 @@
-# ![](img/arrow.svg) Release notes
-
-## Version 1.0 / 2020-08-16
-
-- Initial release
--- src/web-server/web-server.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-
-if exist "%JAVA_HOME%\bin\javaw.exe" (
-  start "ShortyCut" /B "%JAVA_HOME%\bin\javaw.exe" -jar web-server.jar
-) else (
-  start "ShortyCut" /B "javaw.exe" -jar web-server.jar
-)
